30e6eaa9fd18ba70431941d945f2f534c7c1622f
yourname <602204582@qq.com>
2019-07-22 16:18:55 +0800

    UI 示例项目

---
+++ src/T0x9B/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="midea-demo-wrapper">
+        <text class="greeting">{{msg}}</text>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg: 'hello, this is T0x9B'
+        }
+    }
+}
+</script>
+<style scoped>
+  .greeting {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+</style>
+
+
---
+++ src/midea-demo/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="midea-demo-wrapper">
+        <text class="greeting">{{msg}}</text>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg: 'hello, this is midea demo'
+        }
+    }
+}
+</script>
+<style scoped>
+  .greeting {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+</style>
+
+
